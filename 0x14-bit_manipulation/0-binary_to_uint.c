cbda837fc5ff86ae759eb863eba43d63e287e56f
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-08-31 18:23:56 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,27 +9,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui = 0;
-	int len, base_two;
+	unsigned int val = 0;
+	int i, base_two;
 
 	if (!b)
 		return (0);
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (i = 0; b[i] != '\0'; i++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i--, base_two = 1; i >= 0; i--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[i] & 1)
 		{
-			ui += base_two;
+			val += base_two;
 		}
 	}
 
-	return (ui);
+	return (val);
 }
