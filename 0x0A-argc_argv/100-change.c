b7bc3b246c99a2ba976037a4c712fa79fdea7e89
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-07 17:01:41 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,23 +10,24 @@
  */
 int main(int argc, char *argv[])
 {
+	int cents;
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = 0;
+	int i = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = 0;
-	int i = 0;
 
 	while (cents > 0)
 	{
