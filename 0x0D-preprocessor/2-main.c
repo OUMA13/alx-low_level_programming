24a627a07a50bca551f3bc1947a03d05a4498fc6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-10 13:51:22 -0700

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,6 @@
-#include <stdio.>
+#include <stdio.h>
 
-/**
- * main - print name of of this source file
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
+int main() {
+    printf("%s\n", __FILE__);
+    return 0;
 }
