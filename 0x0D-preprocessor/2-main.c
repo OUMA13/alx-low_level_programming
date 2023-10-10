c9782f297f1813f3c521f39cf3ee5349c2186545
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-10 13:39:27 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <stdio.>
 
-int main() {
-    printf("%s\n", __FILE__);
-    return 0;
+/**
+ * main - print name of of this source file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
