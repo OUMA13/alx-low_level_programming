c863cc536ea683916cc4f9e697a392fa668f89ae
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-10 15:37:09 -0700

    commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a big kitty world
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
