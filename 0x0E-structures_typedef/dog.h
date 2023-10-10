639d1a2cd80c6773039e2bf1988871a6173b1cbb
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-10 15:19:30 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
- * @owner:owner's name
+ * @owner: owner's name
  *
  * Description: just a lone dog struct in a big kitty world
  * */
