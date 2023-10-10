050642de6b61e43caf21542429340bcfb2d305e4
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-10 14:31:44 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
-int main(void)
-{
-    struct dog my_dog;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-
-    return 0;
-}
+#endif
 
