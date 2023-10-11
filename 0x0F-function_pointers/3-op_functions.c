987ea7762262bed0e6b1cf41a5c82d1b5dcb91b6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-11 15:29:37 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first int
+ * @b; the second int
+ *
+ * Returne: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: the first int
+ * @b: the srcond int
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
