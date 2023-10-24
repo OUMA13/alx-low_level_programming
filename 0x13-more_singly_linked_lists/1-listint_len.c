90b65fc954e4640dbb46d48bc5e83f68d0340363
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-24 14:10:20 -0700

    lists.h

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t i = 0;
-
-        while (h)
+	size_t i = 0;
+	
+	while (h)
         {
-                h = h->next;
-                i++;
+		h = h->next;
+		i++;
         }
-
-        return (i);
+	return (i);
 }
