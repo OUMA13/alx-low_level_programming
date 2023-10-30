eef03a98f66f4327574c3a1e25546dcb077c9052
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-30 15:41:04 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,20 +19,20 @@ int _strlen(char *s)
 }
 
 /**
- * creat_file - creates a file
+ * create_file - creates a file
  * @filename: name of file to create
  * @text_content: text to write
  *
  * Return: 1 on success 0 on failutre
  */
-int creat_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, S_IRUSE | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
