5ce16911ca4ebbbac1cb8ae2b7ec54da8bd74f6f
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-10-30 16:02:55 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,8 +9,8 @@
 #define READ_BUF_SIZE 1024
 
 int _putchar(char c);
-ssize_t read_textfile(const char *flename, size_t letters);
-int creat_file(const char *filename, char *text_content);
-int append_text_tofile(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
