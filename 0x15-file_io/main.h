72d5d1e7f4cfddc8556dddd8ac04384b1513d7cb
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2023-11-03 14:05:24 -0700

    a

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
-#define READ_BUF_SIZE 1,024
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    int res;
 
-int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-
-#endif
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
+}
